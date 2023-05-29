3fcec634ab09d9767eecc2076d4ca8388c287c28
Christopher Bengtsson <christopher.bengtsson@trustly.com>
2023-05-29 16:21:07 +0200

    fallback if no open coupong

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -5,12 +5,16 @@ import { DrawEvent } from "../api";
 
 export const Main = observer(function Main() {
   const store = useMainStore();
+  const draws = store.draws?.at(0);
+  const events = draws?.drawEvents;
+
   return (
     <Layout>
-      <h1>{store.draws?.at(0)?.regCloseDescription}</h1>
-      <h4>Last updated {store.lastUpdated?.toLocaleTimeString()}</h4>
-
-      <Table events={store.draws?.at(0)?.drawEvents ?? ([] as DrawEvent[])} />
+      <h1>
+        {draws?.regCloseDescription ?? "Stryktipset öppnar tisdag kl. 07:00."}
+      </h1>
+      <h4>Senast uppdaterad {store.lastUpdated?.toLocaleTimeString()}</h4>
+      {events && <Table events={events} />}
     </Layout>
   );
 });
